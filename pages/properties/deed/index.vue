4ed5101936ce64a82da3e2fc1c5f417dffad4e33
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2023-02-16 09:53:57 +0600

    property deed fixing

--- pages/properties/deed/index.vue
+++ pages/properties/deed/index.vue
@@ -1,20 +1,18 @@
 <template>
   <div>
-    <div class="card">
+    <div v-if="isloading" class="text-center">
+      <p style="font-size: 20px;">Loading...</p>
+    </div>
+    <div v-else class="card">
       <div class="card-header d-flex justify-content-between align-items-center">
         <h5 class="card-title m-0">Deed List</h5>
-
-        <!--        <nuxt-link :to="{ name: 'users-landlords-create' }" class="btn btn-info">-->
-        <!--          <font-awesome-icon icon="fa-solid fa-plus" />-->
-        <!--          Add Lease-->
-        <!--        </nuxt-link>-->
       </div>
 
       <div class="card-body">
         <div class="search d-flex justify-content-between align-items-center">
           <div class="form-group">
             <input class="form-control custom-form-control" type="text" v-model="tableData.search"
-                   placeholder="Search Table" @input="getData()">
+              placeholder="Search Table" @input="getData()">
           </div>
           <div class="form-group">
             <select class="form-control custom-select-form-control" v-model="tableData.length" @change="getData()">
@@ -23,50 +21,43 @@
           </div>
         </div>
         <DataTable id="dataTable" :columns="columns" :sortKey="sortKey" :sortOrders="sortOrders" @sort="sortBy"
-                   class="">
+          class="">
           <tbody>
-          <tr v-for="(value,i) in values" :key="value.id">
-          <tr v-for="(value,i) in values" :key="value.id">
-            <td>{{ i + 1 }}</td>
-            <td>{{ value.landlord.name }}</td>
-            <td>{{ value.tenant.name }}</td>
-            <td>{{ value.property.name }}</td>
-            <td>
-              <div v-if="value.property.sale_type == 1"> Rent</div>
-              <div v-if="value.property.sale_type == 2"> Sale</div>
-            </td>
-            <td>{{ value.property_ad.rent_amount }}</td>
-            <td>
-              <select @change="statusChange({id:value.id, status: $event.target.value})" name="" id="status"
-                      class="form-control custom-select-form-control">
-                <option :selected="value.status == 0" value="0">Pending</option>
-                <option :selected="value.status == 1" value="1">Send to landlord</option>
-                <option :selected="value.status == 2" value="2">Completed</option>
-                <option :selected="value.status == 3" value="3">Decline</option>
-              </select>
-            </td>
-            <td>
-              <nuxt-link :to="{name:'properties-id-show',params: { id: value.property_id }}" rel="tooltip"
-                         class="btn btn-sm btn-info btn-simple"
-                         title="View property">
-                <font-awesome-icon icon="fa-solid fa-hotel"/>
-              </nuxt-link>
-              <!--              <nuxt-link :to="{name:'properties-id-edit',params: { id: value.id }}" rel="tooltip"-->
-              <!--                         class="btn btn-sm btn-success btn-simple"-->
-              <!--                         title="Edit">-->
-              <!--                <font-awesome-icon icon="fa-solid fa-edit"/>-->
-              <!--              </nuxt-link>-->
-              <b-button class="btn btn-sm btn-danger" @click="deleteItem(value.id)">
-                <font-awesome-icon icon="fa-solid fa-trash"/>
-              </b-button>
-            </td>
-          </tr>
+            <tr v-for="(value, i) in values" :key="value.id">
+              <td>{{ i + 1 }}</td>
+              <td>{{ value.landlord.name }}</td>
+              <td>{{ value.tenant.name }}</td>
+              <td>{{ value.property.name }}</td>
+              <td>
+                <div v-if="value.property.sale_type == 1"> Rent</div>
+                <div v-if="value.property.sale_type == 2"> Sale</div>
+              </td>
+              <td>{{ value.property_ad.rent_amount }}</td>
+              <td>
+                <select @change="statusChange({ id: value.id, status: $event.target.value })" name="" id="status"
+                  class="form-control custom-select-form-control">
+                  <option :selected="value.status == 0" value="0">Pending</option>
+                  <option :selected="value.status == 1" value="1">Send to landlord</option>
+                  <option :selected="value.status == 2" value="2">Completed</option>
+                  <option :selected="value.status == 3" value="3">Decline</option>
+                </select>
+              </td>
+              <td>
+                <nuxt-link :to="{ name: 'properties-id-show', params: { id: value.property_id } }" rel="tooltip"
+                  class="btn btn-sm btn-info btn-simple" title="View property">
+                  <font-awesome-icon icon="fa-solid fa-hotel" />
+                </nuxt-link>
+
+                <b-button class="btn btn-sm btn-danger" @click="deleteItem(value.id)">
+                  <font-awesome-icon icon="fa-solid fa-trash" />
+                </b-button>
+              </td>
+            </tr>
           </tbody>
         </DataTable>
 
-        <pagination :pagination="pagination"
-                    @prev="getData(pagination.prevPageUrl)"
-                    @next="getData(pagination.nextPageUrl)">
+        <pagination :pagination="pagination" @prev="getData(pagination.prevPageUrl)"
+          @next="getData(pagination.nextPageUrl)">
         </pagination>
 
       </div>
@@ -80,32 +71,27 @@ import Pagination from "@/components/Datatable/Pagination";
 
 export default {
   name: "index",
-  components: {Pagination, DataTable},
+  components: { Pagination, DataTable },
   created() {
     this.getData();
   },
-  computed: {
-    // imageUrl(){
-    //   return `${process.env.APP_ROOT_IMG_URL}/`
-    // }
-  },
-
   data() {
     let sortOrders = {};
     let columns = [
-      {width: '', label: 'Sl', name: 'id'},
-      {width: '', label: 'Landlord', name: 'landlord_id'},
-      {width: '', label: 'Tenant', name: 'tenant_id'},
-      {width: '', label: 'Property', name: 'property_id'},
-      {width: '', label: 'Type', name: 'lease_type'},
-      {width: '', label: 'Amount', name: 'rent_amount'},
-      {width: '', label: 'Status', name: 'status'},
-      {width: '', label: 'Action', name: ''},
+      { width: '', label: 'Sl', name: 'id' },
+      { width: '', label: 'Landlord', name: 'landlord_id' },
+      { width: '', label: 'Tenant', name: 'tenant_id' },
+      { width: '', label: 'Property', name: 'property_id' },
+      { width: '', label: 'Type', name: 'lease_type' },
+      { width: '', label: 'Amount', name: 'rent_amount' },
+      { width: '', label: 'Status', name: 'status' },
+      { width: '', label: 'Action', name: '' },
     ];
     columns.forEach((column) => {
       sortOrders[column.name] = -1;
     });
     return {
+      isloading: true,
       status: '',
       values: [],
       sum: [],
@@ -135,21 +121,18 @@ export default {
   methods: {
     getData(url = 'property/deed/list') {
       this.tableData.draw++;
-      this.$axios.post(url, {params: this.tableData})
+      this.$axios.post(url, { params: this.tableData })
         .then(response => {
           let data = response.data;
           if (this.tableData.draw == data.draw) {
             this.values = data.data.data;
             this.configPagination(data.data);
           }
-        })
-        .catch(errors => {
-          //console.log(errors);
-        }).finally(() => {
-
-      });
+          this.isloading = false;
+        }).catch(error => {
+          alert(error);
+        });
     },
-
     async statusChange(params) {
       await this.$axios.$post('property/deed/change-status/' + params.id, params)
         .then(response => {
@@ -158,17 +141,10 @@ export default {
             message: 'Deed status change successfully!'
           })
           this.getData()
-        })
-        .catch(error => {
-          if (error.response.status == 422) {
-            this.errors = error.response.data.errors
-          } else {
-            alert(error.response.message)
-          }
-        })
+        }).catch(error => {
+          alert(error);
+        });
     },
-
-    // Landloard Delete logic
     async deleteItem(id) {
       let result = confirm("Want to delete?");
 
@@ -184,15 +160,10 @@ export default {
             });
           })
           .catch(error => {
-            if (error.response.status == 422) {
-              this.errors = error.response.data.errors
-            } else {
-              alert(error.response.message)
-            }
-          })
+            alert(error);
+          });
       }
     },
-
     configPagination(data) {
       this.pagination.lastPage = data.last_page;
       this.pagination.currentPage = data.current_page;
@@ -203,7 +174,6 @@ export default {
       this.pagination.from = data.from;
       this.pagination.to = data.to;
     },
-
     sortBy(key) {
       this.sortKey = key;
       this.sortOrders[key] = this.sortOrders[key] * -1;
@@ -214,12 +184,6 @@ export default {
     getIndex(array, key, value) {
       return array.findIndex(i => i[key] == value)
     },
-
-
   }
 }
 </script>
-
-<style scoped>
-
-</style>
